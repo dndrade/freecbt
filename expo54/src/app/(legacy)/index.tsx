05108cb7f1d3ef8c8de4f2428e70883e1d89af28
dndrade <68204412+dndrade@users.noreply.github.com>
2026-07-15 03:47:31 +0000

    [audit] remove dead (legacy)/ screens crashing Expo Go via expo-notifications

--- expo54/src/app/(legacy)/index.tsx
+++
@@ -1,9 +0,0 @@
-import CreateThought from "./thoughts/create";
-
-export default function Index() {
-  // if (__DEV__) {
-  //   const { Redirect } = require("expo-router");
-  //   return <Redirect href="/v2" />;
-  // }
-  return <CreateThought />;
-}
--- expo54/src/app/(legacy)/lock-update.tsx
+++
@@ -1,6 +0,0 @@
-import LockScreen from "@/src/legacy/screen/LockScreen";
-import React from "react";
-
-export default function LockUpdateScreen(): React.JSX.Element {
-  return <LockScreen isSettingCode={true} />;
-}
