e9149611cdfcab38b991c289414a58f29076b2fd
dndrade <68204412+dndrade@users.noreply.github.com>
2026-08-16 23:26:31 +0000

    refactor(thoughts): move list and delete UI into features/thoughts

---
+++ expo54/src/features/thoughts/thought-list-screen.tsx
@@ -0,0 +1,79 @@
+import { Routes } from "@/src";
+import { ModelLoadedProps } from "@/src/hooks/use-model";
+import { Action, Model, Thought } from "@/src/model";
+import { Feather } from "@expo/vector-icons";
+import { Link } from "expo-router";
+import React from "react";
+import { ScrollView, Text, TouchableOpacity, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export function ThoughtListScreen(props: ModelLoadedProps) {
+  const { model, dispatch, style: s, translate: t } = props;
+  const list = Model.thoughtsByDate(model);
+  const today = new Date().toDateString();
+  return (
+    <SafeAreaView style={[s.view]}>
+      <ScrollView style={[s.flexCol, s.container]}>
+        {list.length === 0 ? (
+          <Text style={[s.text]}>{t("cbt_list.empty")}</Text>
+        ) : (
+          list.map(([date, thoughts]) => (
+            <React.Fragment key={date}>
+              <Text style={[s.subheader]}>
+                {today === date ? t("cbt_list.today") : date}
+              </Text>
+              {thoughts.map((thought) => (
+                <ThoughtItem
+                  key={thought.uuid}
+                  style={s}
+                  translate={t}
+                  thought={thought}
+                  model={model}
+                  dispatch={dispatch}
+                />
+              ))}
+            </React.Fragment>
+          ))
+        )}
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
+function ThoughtItem(props: ModelLoadedProps & { thought: Thought.Thought }) {
+  const { model, thought, dispatch, style: s, translate: t } = props;
+  const onDelete = () => dispatch(Action.deleteThought(thought.uuid));
+  return (
+    <View style={[s.flexRow, s.justifyBetween, s.m2]}>
+      <TouchableOpacity style={[s.flex1, s.border, s.rounded, s.p2]}>
+        <Link
+          style={[s.flex1, s.flexCol]}
+          href={Routes.thoughtViewV2(thought.uuid)}
+        >
+          <Text style={[s.text]}>
+            {Thought.label(thought, model)}
+            {"\n\n"}
+          </Text>
+          {Thought.emojis(thought) ? (
+            <Text style={[s.text, s.bgCard, s.rounded, s.p1]}>
+              {Thought.emojis(thought)}
+            </Text>
+          ) : (
+            <></>
+          )}
+          <Text style={[s.text]}></Text>
+        </Link>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={[s.selfStart, s.bgCard, s.border, s.rounded, s.p2, s.mx2]}
+        onPress={onDelete}
+      >
+        <Text style={[s.buttonText]}>
+          <Feather
+            name="trash"
+            accessibilityLabel={t("accessibility.delete_thought_button")}
+          />
+        </Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
