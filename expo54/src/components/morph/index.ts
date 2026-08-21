43455541c869198bedd88f6c8baeb3a040b72ad8
dndrade <68204412+dndrade@users.noreply.github.com>
2026-08-21 19:37:06 +0000

    feat(morph): add morph module barrel export

---
+++ expo54/src/components/morph/index.ts
@@ -0,0 +1,8 @@
+export { MORPH_TIMING, useMorphProgress } from "./useMorphProgress";
+export { useCrossfadeLayerStyle } from "./useCrossfadeLayerStyle";
+export type { CrossfadeLayerOptions } from "./useCrossfadeLayerStyle";
+export { useMorphContainerStyle } from "./useMorphContainerStyle";
+export type { MorphContainerOptions } from "./useMorphContainerStyle";
+export { CrossfadeSwap } from "./CrossfadeSwap";
+export type { CrossfadeSwapProps } from "./CrossfadeSwap";
+export { MORPH_PRESETS } from "./presets";
