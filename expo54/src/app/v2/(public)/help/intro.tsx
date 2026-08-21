5da015d8dd6aa34dd6198b3b2167e35035d95571
dndrade <68204412+dndrade@users.noreply.github.com>
2026-08-21 22:03:40 +0000

    refactor(onboarding): thin the intro route, drop dead elm-arch wiring

--- expo54/src/app/v2/(public)/help/intro.tsx
+++ expo54/src/app/v2/(public)/help/intro.tsx
@@ -1,23 +1,5 @@
-import { Routes } from "@/src";
-import { OnboardingScreen } from "@/src/features/onboarding/onboarding-screen";
-import { LoadModel, ModelLoadedProps } from "@/src/hooks/use-model";
-import { Action } from "@/src/model";
-import { useRouter } from "expo-router";
+import { OnboardingScreen } from "@/src/features/onboarding";
 
-export default function Index() {
-  return <LoadModel ready={Onboarding} />;
-}
-
-function Onboarding(props: ModelLoadedProps) {
-  const router = useRouter();
-
-  return (
-    <OnboardingScreen
-      {...props}
-      onSkip={() => {
-        props.dispatch(Action.setExistingUser());
-        router.push(Routes.homeV2());
-      }}
-    />
-  );
+export default function Intro() {
+  return <OnboardingScreen />;
 }
