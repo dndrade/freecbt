140b1c6261c1aa61e7a3efd9339f4978cd736867
dndrade <68204412+dndrade@users.noreply.github.com>
2026-08-25 04:04:47 +0000

    refactor(reminders): bind hook directly to settings and scheduler

--- expo54/src/features/reminders/use-reminders.ts
+++ expo54/src/features/reminders/use-reminders.ts
@@ -1,81 +1,40 @@
-import * as Notifications from "expo-notifications";
 import { Platform } from "react-native";
-import { TranslateFn } from "@/src/i18n/use-i18n";
+import { useSettings } from "@/src/features/settings/hooks/useSettings";
+import type { TranslateFn } from "@/src/i18n/use-i18n";
+import {
+  cancelScheduledReminders,
+  scheduleDailyReminder,
+  type ReminderTime,
+} from "./services/reminderScheduler";
 
 export type Reminders = ReturnType<typeof useReminders>;
+
+// v1 code's reminder-support check.
+// as of 2025/12, expo-notifications doesn't support web: https://docs.expo.dev/guides/using-push-notifications-services/#tips-and-important-considerations
+// TODO: not sure why android wasn't enabled, but wait til the big v2 release is done to enable it
+export const DEFAULT_REMINDER_TIME: ReminderTime = { hour: 20, minute: 0 };
+
 export function useReminders() {
+  const enabled = useSettings((s) => s.settings.reminders);
+  const setReminders = useSettings((s) => s.setReminders);
+
   return {
-    // v1 code's reminder-support check.
-    // as of 2025/12, expo-notifications doesn't support web: https://docs.expo.dev/guides/using-push-notifications-services/#tips-and-important-considerations
-    // TODO: not sure why android wasn't enabled, but wait til the big v2 release is done to enable it
     isSupported: () => Platform.OS === "ios",
-    async enable(t: TranslateFn) {
-      await enable(t);
+    enabled,
+    async enableReminders(
+      time: ReminderTime = DEFAULT_REMINDER_TIME,
+      t: TranslateFn,
+    ) {
+      await scheduleDailyReminder(time, t);
+      setReminders(true);
     },
-    async disable() {
-      await disable();
+    async disableReminders() {
+      await cancelScheduledReminders();
+      setReminders(false);
     },
-    async set(v: boolean, t: TranslateFn) {
-      if (v) {
-        await this.enable(t);
-      } else {
-        await this.disable();
-      }
+    async updateReminderTime(time: ReminderTime, t: TranslateFn) {
+      if (!enabled) return;
+      await scheduleDailyReminder(time, t);
     },
   } as const;
-}
-
-async function disable() {
-  return await Notifications.cancelAllScheduledNotificationsAsync();
-}
-async function enable(t: TranslateFn): Promise<boolean> {
-  await disable();
-  // don't enable without permission
-  const enabled = await registerForLocalNotificationsAsync();
-  if (enabled) {
-    await Notifications.scheduleNotificationAsync({
-      content: {
-        title: t("reminder_notification.intro.title"),
-        body: t("reminder_notification.intro.body"),
-        color: "#F78FB3",
-        // icon: "https://freecbt.erosson.org/static/favicon/favicon.ico",
-        // icon: "https://freecbt.erosson.org/notifications/quirk-bw.png",
-      },
-      trigger: null,
-    });
-    await Notifications.scheduleNotificationAsync({
-      content: {
-        title: t("reminder_notification.1.title"),
-        body: t("reminder_notification.1.body"),
-        color: "#F78FB3",
-        // icon: "https://freecbt.erosson.org/static/favicon/favicon.ico",
-        // icon: "https://freecbt.erosson.org/notifications/quirk-bw.png",
-      },
-      trigger: {
-        type: Notifications.SchedulableTriggerInputTypes.DAILY,
-        channelId: "default",
-      },
-      // { channelId: "default", repeats: true, seconds: 86400 },
-    });
-  }
-  return enabled;
-}
-
-async function registerForLocalNotificationsAsync() {
-  const existing = await Notifications.getPermissionsAsync();
-  if (existing.status !== "granted") {
-    const req = await Notifications.requestPermissionsAsync();
-    if (req.status !== "granted") {
-      return false;
-    }
-  }
-
-  if (Platform.OS === "android") {
-    await Notifications.setNotificationChannelAsync("default", {
-      name: "default",
-      importance: Notifications.AndroidImportance.MAX,
-      vibrationPattern: [0, 250, 250, 250],
-    });
-  }
-  return true;
 }
